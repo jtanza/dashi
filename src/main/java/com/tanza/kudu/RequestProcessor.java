67c9d58379cc62a579bf7ffb4c907a2e0475c911
johntanza <tanzajohn@gmail.com>
2019-10-14 17:57:08 -0400

    adds guava cache as container for responses

--- src/main/java/com/tanza/kudu/RequestProcessor.java
+++ src/main/java/com/tanza/kudu/RequestProcessor.java
@@ -1,34 +1,37 @@
 package com.tanza.kudu;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.AllArgsConstructor;
 
 import java.nio.channels.SelectionKey;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jtanza
  */
 @AllArgsConstructor
 public class RequestProcessor {
+    private static final long CACHE_TTL = 10;
 
-    // FIXME use a cache w/ a TTL
-    private final Map<SelectionKey, Response> map = new HashMap<>();
+    private final Cache<SelectionKey, Response> cache = CacheBuilder.newBuilder()
+        .expireAfterWrite(CACHE_TTL, TimeUnit.MINUTES)
+        .build();
 
     public void processAsync(SelectionKey key, Request request, Handler handler) {
         CompletableFuture
             .supplyAsync(() -> handler.getAction().apply(request))
-            .thenApply(response -> map.put(key, response));
+            .thenAccept(response -> cache.put(key, response));
+        //TODO thenAccept publishEvent
     }
 
     public Optional<Response> getResponse(SelectionKey key) {
-        if (map.containsKey(key)) {
-            Response response = map.get(key);
-            map.remove(key);
-            return Optional.of(response);
+        Response response = cache.getIfPresent(key);
+        if (response != null) {
+            cache.invalidate(key);
         }
-        return Optional.empty();
+        return Optional.ofNullable(response);
     }
 }
